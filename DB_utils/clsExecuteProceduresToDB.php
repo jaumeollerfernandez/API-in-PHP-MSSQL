7d5700aaa203905064fa3e2cbc141b8625132ebe
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-05-04 20:17:53 +0200

    refactoring

--- DB_utils/clsExecuteProceduresToDB.php
+++ DB_utils/clsExecuteProceduresToDB.php
@@ -20,40 +20,23 @@ class clsExecuteProceduresToDB implements ControllerDataBaseInterface{
 
         
         $CheckIfItsMatrix = $this->_CheckIfItsMatrix($Params);
-        var_dump($CheckIfItsMatrix);
 
         switch($CheckIfItsMatrix){
             case true:
-                for($i = 0; $i < count($Params); $i++){
-                    $ConcatenatedString = $this->_PrepareStringToPrepareProcedure($NameProcedure, $Params);
-                    $this->prepareProcedure($ConcatenatedString, $Params[$i], count($Params[0]));
-                    array_push($this->_ProcedureQueue, $this->PreparedProcedure);
-                }
-                try{
-                    print_r(count($this->_ProcedureQueue));
-                    for($j = 0; $j < count($this->_ProcedureQueue); $j++){
-                        print_r($Params[$j]);
-                        $this->BindParamToProcedure($Params[$j]);
-                        $this->executeProcedure($this->PreparedProcedure);
-                    }
-                }catch(PDOException $error){
-                    echo('<br>');
-                    echo($error);
-                }
+
+                $this->_PrepareProcedureQueue($NameProcedure, $Params);
+                $this->_BindParamsAndExecuteProcedureQueue($Params);
+
                 break;
+
             case false:
                 if (count($Params) > 0){
-                    $ConcatenatedString = $this->_PrepareStringToPrepareProcedure($NameProcedure, $Params);
-                    $this->prepareProcedure($ConcatenatedString, $Params, 0);
-                    $this->BindParamToProcedure($Params);
-                    $this->executeProcedure($this->PreparedProcedure);
+                   $this->_PrepareProcedureWithParams($NameProcedure, $Params);
                 }else{
-                    $this->prepareProcedure($NameProcedure, [], 0);
-                    $this->executeProcedure($this->PreparedProcedure);
-                    $this->fetchExecutionProcedure();
-                    $this->RenderXML();
+                    $this->_PrepareProcedureWithoutParams($NameProcedure);
                 }
                 break;
+
             default:
                 echo('Error in your Procedure Call');
         }
@@ -61,9 +44,66 @@ class clsExecuteProceduresToDB implements ControllerDataBaseInterface{
 
     }
 
+    function _PrepareProcedureWithParams($NameProcedure, $Params){
+        $ConcatenatedString = $this->_PrepareStringToPrepareProcedure($NameProcedure, $Params);
+        $this->prepareProcedure($ConcatenatedString, $Params, 0);
+        $this->BindParamToProcedure($Params);
+        $this->executeProcedure($this->PreparedProcedure);
+    }
+
+    function _PrepareProcedureWithoutParams($NameProcedure){
+        $this->prepareProcedure($NameProcedure, [], 0);
+        $this->executeProcedure($this->PreparedProcedure);
+        $this->fetchExecutionProcedure();
+        $this->RenderXML();
+    }
+
+    function _PrepareProcedureQueue($NameProcedure, $Params){
+        for($i = 0; $i < count($Params); $i++){
+            $ConcatenatedString = $this->_PrepareStringToPrepareProcedure($NameProcedure, $Params);
+            $this->prepareProcedure($ConcatenatedString, $Params[$i], count($Params[0]));
+            $this->_InsertProcedureIntoQueue($this->PreparedProcedure);
+        }
+    }
+
+    function _BindParamsAndExecuteProcedureQueue(Array $Params){
+        try{
+            for($j = 0; $j < count($this->_ProcedureQueue); $j++){
+                print_r($Params[$j]);
+                $this->BindParamToProcedure($Params[$j]);
+                $this->executeProcedure($this->PreparedProcedure);
+            }
+        }catch(PDOException $error){
+            echo($error);
+        }
+    }
+    
     function _CheckIfItsMatrix($Matrix){
         $result = is_array($Matrix[0]);
         return $result;
+    }
+
+    function prepareProcedure(string $name_procedure, array $params = []): void
+    {
+        $this->ProcedureName = $name_procedure;
+        $this->PreparedProcedure = $this->DBconnection->prepare($this->ProcedureName);
+    }
+
+    function BindParamToProcedure($ArrayOfParams){
+        $this->_id = 1;
+        for($j = 1; $j <= count($ArrayOfParams); $j++){
+            $this->PreparedProcedure->bindParam($this->_id,$ArrayOfParams[$j-1]);
+            $this->_id++;
+        }
+    }   
+
+    function executeProcedure($Procedure): void
+    {
+        $Procedure->execute();
+    }
+
+    function _InsertProcedureIntoQueue($Procedure){
+        array_push($this->_ProcedureQueue, $Procedure);
     }
 
     function _PrepareStringToPrepareProcedure(string $name_procedure, $ArrayOfParams){
@@ -101,24 +141,7 @@ class clsExecuteProceduresToDB implements ControllerDataBaseInterface{
         return implode("",$result);
     }
     
-    function prepareProcedure(string $name_procedure, array $params = []): void
-    {
-        $this->ProcedureName = $name_procedure;
-        $this->PreparedProcedure = $this->DBconnection->prepare($this->ProcedureName);
-    }
-
-    function BindParamToProcedure($ArrayOfParams){
-        $this->_id = 1;
-        for($j = 1; $j <= count($ArrayOfParams); $j++){
-            $this->PreparedProcedure->bindParam($this->_id,$ArrayOfParams[$j-1]);
-            $this->_id++;
-        }
-    }   
     
-    function executeProcedure($Procedure): void
-    {
-        $Procedure->execute();
-    }
     
     function fetchExecutionProcedure(): void
     {   
--- index.php
+++ index.php
@@ -6,8 +6,8 @@ include_once __DIR__."/DB_utils/clsExecuteProceduresToDB.php";
 $a = new clsConnectToDB("172.17.0.1","14333","shop","SA","@Asix13021997");
 $a->initConnection();
 $b = new clsExecuteProceduresToDB($a->getPDODB());
-// $b->CallProcedure("insert_employee",[['testc',10,'a'],['testD',11,'b'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC']]);
-$b->CallProcedure("insert_employee", ['pingo',1000,'ICC']);
+$b->CallProcedure("insert_employee",[['testc',10,'a'],['testD',11,'b'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC']]);
+// $b->CallProcedure("insert_employee", ['pingo',1000,'ICC']);
 
 // $b->CallProcedure("get_products", [], 'none');
 ?>
