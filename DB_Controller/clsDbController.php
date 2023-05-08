e0abc16978751ae8fb720881fdcd4c8c5930755d
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-05-08 20:08:05 +0200

    update

--- DB_Controller/clsDbController.php
+++ DB_Controller/clsDbController.php
@@ -1,12 +1,22 @@
 <?php
 
+include_once __DIR__."/../DB_utils/clsConnectToDB.php";
+include_once __DIR__."/../DB_utils/clsExecuteProceduresToDB.php";
+
 class clsDbController{
+    private clsConnectToDB $DBconnection;
+    private clsExecuteProceduresToDB $ProcedureExecute;
 
-    function __construct()
-    {
-        
+    function __construct(){}
+
+    function AddMSSQLConnectionToDB($sql, $port, $DBname, $user, $pwd){
+        $this->DBconnection = new clsConnectToDB($sql, $port, $DBname, $user, $pwd);
+        $this->DBconnection->initConnection();
+        $this->ProcedureExecute = new clsExecuteProceduresToDB($this->DBconnection->getPDODB());
     }
-
+    function ExecuteProcedure($procedure, $params){
+        $this->ProcedureExecute->CallProcedure($procedure, $params);
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php 
 
-include_once __DIR__."/DB_utils/clsConnectToDB.php";
-include_once __DIR__."/DB_utils/clsExecuteProceduresToDB.php";
+include_once __DIR__."/DB_Controller/clsDbController.php";
 
-$a = new clsConnectToDB("172.17.0.1","14333","shop","SA","@Asix13021997");
-$a->initConnection();
-$b = new clsExecuteProceduresToDB($a->getPDODB());
-// $b->CallProcedure("insert_employee",[['testc',10,'a'],['testD',11,'b'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC']]);
-// $b->CallProcedure("insert_employee", ['pingo',1000,'ICC']);
+$a = new clsDbController();
+$a->AddMSSQLConnectionToDB("172.17.0.1","14333","shop","SA","@Asix13021997");
+$a->ExecuteProcedure("insert_employee",[['testc',10,'a'],['testD',11,'b'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC'],['Quimo',1000,'ICC']]);
 
-// $b->CallProcedure("get_products", []);
 ?>
