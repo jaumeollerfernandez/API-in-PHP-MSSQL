17492eafd551431255a5157f3937c951dcbe1dfa
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-05-11 21:16:16 +0200

    update

--- DB/SAP-DB/DML/function.sf_sap_user_validate_pwd.sql
+++ DB/SAP-DB/DML/function.sf_sap_user_validate_pwd.sql
@@ -1,30 +1,3 @@
-
-USE WS_API_07;
-CREATE function sf_sap_user_exists(@user_id nvarchar(255))
-RETURNS INT
-as
-
-begin
-DECLARE @response int;
-
-SET @response = 0;
-IF ( (SELECT count(*) from _sap_users where user_id = @user_id) = 1)
-
-SET @response = -1;
-
-RETURN @response
-
-/********************************* TEST UNITARIO*********************************
-DECLARE @ret INT;
-EXEC @ret = sf_if_exist_user @user_id = 'ely@gmail.com' ;
-select @ret;
-*********************************************************************************/
-END
-go
-
-
-
-
 CREATE   function  sf_sap_user_validate_pwd(@user_id nvarchar(255),@pwd nvarchar(255))
 RETURNS INT
 as
