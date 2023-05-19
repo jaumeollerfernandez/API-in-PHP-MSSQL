4252e07ff8fb11d0e6fdd66646f28ed4b14f7b85
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-05-19 16:47:03 +0200

    udpate

--- SecurityController/clsSecurityController.php
+++ SecurityController/clsSecurityController.php
@@ -12,18 +12,18 @@ class clsSecurityController{
         
         switch(strtolower($this->action)){
             case 'login':
-                $user = new clsUser("172.17.0.1","14333","TEST_WS_API_07","SA","@Asix13021997");
+                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
                 $user->GenerateConnectionToDB();
                 $user->DetectCookieOnClient();
                 $user->ExecuteAction('login', $this->params);
                 break;
             case 'logout':
-                $user = new clsUser("172.17.0.1","14333","TEST_WS_API_07","SA","@Asix13021997");
+                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
                 $user->GenerateConnectionToDB();
                 $user->ExecuteAction('logout', $this->params);
                 break;
             case 'register':
-                $user = new clsUser("172.17.0.1","14333","TEST_WS_API_07","SA","@Asix13021997");
+                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
                 $user->GenerateConnectionToDB();
                 $user->ExecuteAction('register', $this->params);
                 break;
