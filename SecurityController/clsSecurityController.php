e76a19a21ad05a1545b8b130f4c0bc55383e505c
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-05-24 15:18:20 +0200

    refactored first part

--- SecurityController/clsSecurityController.php
+++ SecurityController/clsSecurityController.php
@@ -7,34 +7,52 @@ class clsSecurityController{
 
     function __construct(){}
 
+    /**
+     * Public Functions
+     */
+
     public function ExecuteAction($action, $params){
         $this->action = $action;
         $this->params = $params;
         
         switch(strtolower($this->action)){
             case 'login':
-                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
-                $user->GenerateConnectionToDB();
-                // $user->DetectCookieOnClient();
-                $user->ExecuteAction('login', $this->params);
+                $this->_TryLogin();
                 break;
                 case 'logout':
-                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
-                $user->GenerateConnectionToDB();
-                $user->ExecuteAction('logout', $this->params);
+                $this->_TryLogout();
                 break;
             case 'register':
-                $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
-                $user->GenerateConnectionToDB();
-                $user->ExecuteAction('register', $this->params);
+                $this->_TryRegister();
                 break;
             }
             
-            $this->XMLresponse = $user->GetXMLresponseFromDB();
-    }
-
+        }
     public function ObtainXMLResponse(){
         return $this->XMLresponse;
+    }
+
+    /**
+     * Private Functions
+     */
+
+    protected function _TryLogin(){
+        $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
+        $user->GenerateConnectionToDB();
+        $user->ExecuteAction('login', $this->params);
+        $this->XMLresponse = $user->GetXMLresponseFromDB();
+    }
+    protected function _TryLogout(){
+        $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
+        $user->GenerateConnectionToDB();
+        $user->ExecuteAction('logout', $this->params);
+        $this->XMLresponse = $user->GetXMLresponseFromDB();
+    }
+    protected function _TryRegister(){
+        $user = new clsUser("172.17.0.1","14333","WS_API_07","SA","@Asix13021997");
+        $user->GenerateConnectionToDB();
+        $user->ExecuteAction('register', $this->params);
+        $this->XMLresponse = $user->GetXMLresponseFromDB();
     }
 
     
